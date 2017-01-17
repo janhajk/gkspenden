a7270860e6be518e224f63be5573781abee10523
Jan <janhajk@gmail.com>
2017-01-17 12:23:45 +0000

    reverse

--- injections.js
+++ injections.js
@@ -2,24 +2,23 @@ document.addEventListener("DOMContentLoaded", function() {
    (function(){
       var anrede = document.getElementById('edit-field-profil-anrede-value');
       anrede.onchange = function(){
+         var anreden = {
+            'Herrn' : 'Sehr geehrter Herr %Nachname',
+            'Frau': 'Sehr geehrte Frau %Nachname',
+            'Herrn und Frau': 'Sehr geehrte Frau %Nachname, sehr geehrter Herr %Nachname',
+            'Familie': 'Sehr geehrte Famlie %Nachname',
+            'default': 'Sehr geehrte Damen und Herren'
+         };
          var value = this.value;
          var Nachname = document.getElementById('edit-field-profil-nachname-0-value').value;
          var klausel = '';
-         if (value==='Herrn') {
-            klausel = 'Sehr geehrter Herr ' + Nachname;
-         }
-         else if (value==='Frau') {
-            klausel = 'Sehr geehrte Frau ' + Nachname;
-         }
-         else if (value==='Herrn und Frau') {
-            klausel = 'Sehr geehrte Frau ' + Nachname + ', sehr geehrter Herr ' + Nachname;
-         }
-         else if (value==='Familie') {
-            klausel = 'Sehr geehrte Familie ' + Nachname;
+         if (anreden[value]!===undefined) {
+            klausel = anreden[value];
          }
          else {
-            klausel = 'Sehr geehrte Damen und Herren';
+            klausel = anreden.default;
          }
+         klausel = klausel.replace(/%Nachname/g, Nachname);
          var fAnrede = document.getElementById('edit-field-profil-briefanredespez-0-value');
          if (fAnrede.value==='') fAnrede.value = klausel;
       };
